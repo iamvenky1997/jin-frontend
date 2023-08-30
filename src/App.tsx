947d3156202f1236c21d09e2d0b80f9d93530c5f
venkateshgali97 <venkateshgali97@gmail.com>
2023-08-30 06:41:45 +0530

    Profile componenet created

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AuthPage from './Components/AuthPage';
+import Profile from './Components/Profile';
 
 const App = () =>{
   return(
-    <AuthPage />
+    // <AuthPage />
+    <Profile />
+
   )
 }
 
